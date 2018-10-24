626c50d34dbcd83ee32caa4ef6b122dbc0330bae
Ben Baker <benjamin.baker@utexas.edu>
2018-10-23 22:56:15 -0400

    added syntax highlighting for odd lines

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,26 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "python-coverage" is now active!');
+    console.log();
+
+    const coveredLineDecoration = vscode.window.createTextEditorDecorationType({
+        backgroundColor: 'red',
+		borderWidth: '1px',
+		borderStyle: 'solid',
+		overviewRulerColor: 'blue',
+		overviewRulerLane: vscode.OverviewRulerLane.Right,
+		light: {
+			// this color will be used in light color themes
+			borderColor: 'darkblue'
+		},
+		dark: {
+			// this color will be used in dark color themes
+			borderColor: 'lightblue'
+        },
+        
+    });
+
+    
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -17,9 +36,39 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
+        const linesToHighlight: vscode.DecorationOptions[] = [];
+
+        let activeEditor = vscode.window.activeTextEditor;
+
+        if(activeEditor){
+
+            console.log('active editor')
+
+            for (var i = 0; i < activeEditor.document.lineCount; i++){
+                if(i % 2){
+
+                    const startPos = new vscode.Position(i, 0);
+                    const endPos = new vscode.Position(i+1, 0);
+                    linesToHighlight.push(
+                        { range: new vscode.Range(startPos, endPos), hoverMessage: 'odd line'}
+                    );
+                }
+            }
+
+            activeEditor.setDecorations(coveredLineDecoration, linesToHighlight);
+        }
+
+        if(!vscode.workspace.rootPath){
+            vscode.window.showInformationMessage('You need to open the folder with your Python project.');
+        }else{
+            console.log(vscode.workspace.rootPath);
+        }
+
         // Display a message box to the user
+        //console.log(activeEditor)
         vscode.window.showInformationMessage('Hello World!');
     });
+
 
     context.subscriptions.push(disposable);
 }
